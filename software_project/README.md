ddfed4e2cf2472fab5301047754cd439603b09fb
Abigail Thomas <agthomas084@gmail.com>
2025-09-17 13:32:47 -0400

    Add calculator module and gitignore

---
+++ software_project/README.md
@@ -0,0 +1,5 @@
+Project Title: My Software Engineering Project
+Abigail Thomas
+1293926
+
+This project will contain a simple calculator.
---
+++ software_project/calculator.py
@@ -0,0 +1,17 @@
+import math
+
+
+def add(n1, n2):
+    return n1 + n2
+    
+
+def subtract(n1, n2):
+    return n1 - n2
+    
+
+def multiply(n1, n2):
+    return n1 * n2
+    
+def divide(n1, n2):
+    return n1 / n2
+    
